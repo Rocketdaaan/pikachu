f1cfec9b410a6631b5420e042f349e5ded9e8ab4
nagao <kousuke.nagao0515@gmail.com>
2016-09-18 07:42:09 +0000

    ティーチャーできたよ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true
+	validates :age, presence: true
+	validates :area, presence: true
+	validates :sex, presence: true
+	validates :skill, presence: true
+	validates :salary, presence: true
+	validates :career, presence: true
+	validates :comment, presence: true
 end
--- db/migrate/20160904061617_create_users.rb
+++ db/migrate/20160904061617_create_users.rb
@@ -1,10 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :user_id
-      t.string :password
-      t.string :user_name
-      t.string :mail_address
+      t.string :name
+      t.string :age
+      t.string :area
+      t.string :sex
+      t.string :skill
+      t.string :salary
+      t.string :career
+      t.string :comment
 
       t.timestamps null: false
     end
