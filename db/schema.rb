ca865e38d3741eac42660d290e0c714e15f09e0e
shun <khf@ezweb.ne.jp>
2016-09-18 05:53:06 +0000

    先生の新規登録と一覧gamennsakusei

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160828071236) do
+ActiveRecord::Schema.define(version: 20160904061617) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
@@ -24,9 +23,6 @@ ActiveRecord::Schema.define(version: 20160828071236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-ActiveRecord::Schema.define(version: 20160828064700) do
-
 
   create_table "students", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -63,5 +59,14 @@ ActiveRecord::Schema.define(version: 20160828064700) do
 
   add_index "teachers", ["email"], name: "index_teachers_on_email", unique: true
   add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "password"
+    t.string   "user_name"
+    t.string   "mail_address"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
